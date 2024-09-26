d9ed8e73d648a4c9629ada54d4e5a3fc8436a137
Bleb1k <viktor.masnikov17283940@gmail.com>
2024-09-26 16:13:47 +0900

    Remove unnecessary comments from helpers.d.ts

--- types/helpers.d.ts
+++ types/helpers.d.ts
@@ -5,31 +5,6 @@ type StringLiteral<T extends string> = readonly T
 
 type Enum<T> = T extends readonly string[] ? { [key in T[number]]: number } : T[number]
 
-// export function _enum<T>(
-// 	...arr: (T extends readonly string[]
-// 		? { readonly [P in T[number] as T[P]]: P }
-// 		: { readonly [P in keyof T]: T[P] })[]
-// ): Enum<T> {
-// 	console.log(Object.entries(arr))
-// 	return Object.freeze(
-// 		Object.fromEntries(
-// 			arr.length === 1 && typeof arr[0] === "object"
-// 				? Object.entries(arr[0]).flatMap(([a, b]) => [
-// 						[a, b],
-// 						[b, a],
-// 				  ])
-// 				: arr
-// 						.map(a => [a, enumItem()])
-// 						.flatMap(([a, b]) => [
-// 							[a, b],
-// 							[b, a],
-// 						])
-// 		)
-// 	)
-// }
-
-// let a = _enum("foo", "bar", "baz")
-// let a = _enum({ foo: 0, bar: 1, baz: 2 })
 let a = ["foo", "bar", "baz"] as const // js dont have 'as const'
 type A = Enum<typeof a>
 
